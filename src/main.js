a9c775987c5361111e004810bc5c1ecff9566f28
Andrew-232 <ahern232@ucsc.edu>
2025-11-22 00:38:47 +0000

    Added player movement controls!
    I created a new file called Player.js where I created the player movement controls which are WASD for movement and Space for jumping.
    
    I rotated the player in main.js to be facing the camera.
    
    I made changes so the camera is focused on the player and will move with the player.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Phaser, { Loader } from 'phaser';
 import { enable3d, Canvas, Scene3D, THREE } from '@enable3d/phaser-extension';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Player } from './Player'; 
 
 export class MainScene extends Scene3D {
   constructor() {
     super({ key: 'MainScene' });
+    this.player = null; 
   }
 
   init() {
@@ -12,24 +14,31 @@ export class MainScene extends Scene3D {
   }
 
   create() {
-    this.warpSpeed()
+    this.warpSpeed();
+    this.third.physics.debug?.enable();
 
-    this.third.physics.debug?.enable()
-
+    // Player
     this.third.load.gltf('src/assets/chip.glb').then((gltf) => {
       const chip = gltf.scene;
 
-      chip.scale.set(1, 1, 1);      // Make it 2x bigger
+      chip.scale.set(1, 1, 1);      // Make it 2x bigger  
       chip.position.set(9, 2, 0);   // Drop it from height of 5
-      this.third.add.existing(chip);
-      this.third.physics.add.existing(chip, { shape: 'convex'});
-    })
+      chip.rotation.y = Math.PI;    // Fliped the player to face the camera
+      this.third.add.existing(chip); 
+      
+      this.third.physics.add.existing(chip, { shape: 'convex', mass: 1 });
+
+      // Initializes the Player Controller
+      this.player = new Player(this, chip);
+      
+      // Makes the camera follow the player
+      this.third.camera.lookAt(chip.position);
+    });
 
     this.third.load.gltf('src/assets/cube.glb').then((gltf) => {
       const helper = gltf.scene;
-
-      helper.scale.set(1, 1, 1);      // Make it 2x bigger
-      helper.position.set(5, 2, 0);   // Drop it from height of 5
+      helper.scale.set(1, 1, 1);      
+      helper.position.set(5, 2, 0);   
       this.third.add.existing(helper);
       this.third.physics.add.existing(helper, { 
         shape: 'box',
@@ -37,24 +46,22 @@ export class MainScene extends Scene3D {
         depth: 2,
         height: 2,
       });
-    })
+    });
 
     this.third.load.gltf('src/assets/flag.glb').then((gltf) => {
       const flag = gltf.scene;
-
-      flag.scale.set(1, 1, 1);      // Make it 2x bigger
-      flag.position.set(-9, 1, 0);   // Drop it from height of 5
+      flag.scale.set(1, 1, 1);     
+      flag.position.set(-9, 1, 0);  
       this.third.add.existing(flag);
       this.third.physics.add.existing(flag, { 
         shape: 'convex',
       });
-    })
+    });
 
     this.third.load.gltf('src/assets/platform.glb').then((gltf) => {
       const platform1 = gltf.scene;
-
-      platform1.scale.set(1, 1, 1);      // Make it 2x bigger
-      platform1.position.set(6, 1, 0);   // Drop it from height of 5
+      platform1.scale.set(1, 1, 1);      
+      platform1.position.set(6, 1, 0);   
       this.third.add.existing(platform1);
       this.third.physics.add.existing(platform1, { 
         shape: 'box',
@@ -62,13 +69,12 @@ export class MainScene extends Scene3D {
         height: 2,
         depth: 2,
       });
-    })
+    });
 
     this.third.load.gltf('src/assets/platform.glb').then((gltf) => {
       const platform2 = gltf.scene;
-
-      platform2.scale.set(1, 1, 1);      // Make it 2x bigger
-      platform2.position.set(-6, 1, 0);   // Drop it from height of 5
+      platform2.scale.set(1, 1, 1);      
+      platform2.position.set(-6, 1, 0);   
       this.third.add.existing(platform2);
       this.third.physics.add.existing(platform2, { 
         shape: 'box',
@@ -76,7 +82,17 @@ export class MainScene extends Scene3D {
         height: 2,
         depth: 2,
       });
-    })
+    });
+  }
+
+  // Update Loop
+  update() {
+    if (this.player) {
+      this.player.update();
+      
+      // Basic camera follow on the player
+      this.third.camera.lookAt(this.player.object.position);
+    }
   }
 }
 
@@ -93,7 +109,7 @@ const config = {
     ammo: {
       gravity: { x: 0, y: -9.81, z: 0 },
     },
-    debug: true  // <--- This enables the wireframes
+    debug: true // This enables the wireframes
   },
   scene: [MainScene],
   ...Canvas(),
